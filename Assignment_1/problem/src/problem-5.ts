756a79abc27b24bb2aa65e8db062bd575a379ee4
altaj1 <altaj1019@gmail.com>
2024-11-07 01:01:05 +0600

    solve problem 5

---
+++ Assignment_1/problem/src/problem-5.ts
@@ -0,0 +1,15 @@
+{
+    type Tperson = {
+        name:string;
+        age: number;
+    }
+    const getProperty =  <T , K extends keyof T >(obj: T, key: K): T[K]=>{
+        return obj[key];
+    }
+    const person :Tperson ={
+        name: "Alice", 
+        age:30
+    } 
+    const result = getProperty(person, "name");
+    console.log(result)
+}
