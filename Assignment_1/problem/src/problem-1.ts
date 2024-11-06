97dfefa77ba057d3254a00c6e5a3f5475c98e6d7
altaj1 <altaj1019@gmail.com>
2024-11-06 21:39:23 +0600

    solve problem 1

--- Assignment_1/problem/src/problem-1.ts
+++ Assignment_1/problem/src/problem-1.ts
@@ -1,1 +1,15 @@
-console.log("hedasdfsasdfasdeasdeadsfasdllo world")
+{
+type Tarray = number[];
+const array : Tarray = [1, 2, 3, 4, 5];
+const sumArray =(array: Tarray) : number =>{
+    let result : number = 0;
+    for (let i = 0; i < array.length; i++) {
+        result = result + array[i] ;
+        
+    }
+    return result
+}
+ const result = sumArray(array);
+ console.log(result)
+
+}
