0a518f7afdbad309fd9111bfb67a238e91a3ac76
altaj1 <altaj1019@gmail.com>
2024-11-06 22:43:01 +0600

    solve problem 3

---
+++ Assignment_1/problem/src/problem-3.ts
@@ -0,0 +1,17 @@
+{
+  type Tstring = string;
+  const sentence: Tstring = "TypeScript is great. I love TypeScript!";
+  const word: Tstring = "typescript";
+  const countWordOccurrences = (sentence: Tstring, word: Tstring): number => {
+    let result : number = 0;
+    const arraySentence = sentence.replace(/[!.]/g, "").split(" ");
+    for (let i = 0; i < arraySentence.length; i++) {
+    if (arraySentence[i].toLowerCase() === word.toLocaleLowerCase()) {
+        result = result + 1;
+    }
+    }
+    return result;
+  };
+ const result =  countWordOccurrences(sentence, word);
+ console.log(result)
+}
