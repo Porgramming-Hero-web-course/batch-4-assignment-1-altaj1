fa80d2bceedc381502e92655a3c3e20ec5b1c72f
altaj1 <altaj1019@gmail.com>
2024-11-06 21:58:17 +0600

    solve problem 2

--- Assignment_1/problem/src/problem-2.ts
+++ Assignment_1/problem/src/problem-2.ts
@@ -1,1 +1,23 @@
-console.log("problem 3")
+{
+    type Tarray = number[];
+    const duplicatesArray :Tarray = [1, 2, 2, 3, 4, 4, 5];
+    const removeDuplicates =(array:Tarray) : Tarray =>{
+        const result :Tarray = []
+        for (let i = 0; i < array.length; i++) {
+            let isDuplicates : boolean = false;
+            for (let j = 0; j < array.length; j++) {
+                if (array[i] === result[j]) {
+                    isDuplicates = true;
+                    break;
+                }
+            }
+            if (!isDuplicates) {
+                result.push(array[i])
+            }
+        }
+        return result;
+    }
+   const result =  removeDuplicates(duplicatesArray);
+   console.log(result)
+
+}
