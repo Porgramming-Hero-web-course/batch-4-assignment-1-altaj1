d8b0efa5455c681808a742193acb7cbb9cc0541b
altaj1 <altaj1019@gmail.com>
2024-11-07 15:22:08 +0600

    update problem 3

--- Assignment_1/problem/src/problem-3.ts
+++ Assignment_1/problem/src/problem-3.ts
@@ -1,6 +1,8 @@
 {
   type Tstring = string;
-  const sentence: Tstring = "TypeScript is great. I love TypeScript!";
+//   const sentence: Tstring = "TypeScript is great. I love TypeScript!";
+  const sentence: Tstring = "I love typescript";
+//   const word: Tstring = "typescript";
   const word: Tstring = "typescript";
   const countWordOccurrences = (sentence: Tstring, word: Tstring): number => {
     let result : number = 0;
