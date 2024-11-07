aca5f17e09d60e01e9b66d29cfcc725779af1d19
altaj1 <altaj1019@gmail.com>
2024-11-07 10:58:53 +0600

    solve porblem 8

---
+++ Assignment_1/problem/src/problem-8.ts
@@ -0,0 +1,23 @@
+{
+    type Tperson = {
+        name:string,
+        age:number,
+        email: string
+    }
+    const validateKeys = <T extends object>(obj: T, keys:(keyof T)[]):boolean =>{
+       
+        for (const key of keys) {
+            if (!(key in obj)) {
+                return false;
+            }
+        }
+        return true
+    }
+    const person:Tperson = {
+        name:"Alice",
+        age:25,
+        email:"alice@example.com"
+    }
+    const result = validateKeys(person, ["name", "age"])
+    console.log(result)
+}
